2001d5e45cd2d13b915dc67fb949a92941e1c940
andreas8ak <andreaskounavis05@gmail.com>
2026-04-17 20:19:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+AOS.init({
+  duration: 850,
+  once: true,
+  offset: 70
+});
+
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.site-nav');
+
+if (toggle && nav) {
+  toggle.addEventListener('click', () => {
+    const isOpen = nav.classList.toggle('open');
+    toggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
+  });
+
+  nav.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      nav.classList.remove('open');
+      toggle.setAttribute('aria-expanded', 'false');
+    });
+  });
+}
